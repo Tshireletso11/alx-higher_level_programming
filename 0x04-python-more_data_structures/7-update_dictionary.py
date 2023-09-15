797902c5d325b2846157eaa660f0abaff90b8577
tshireletso11 <tshireletsobazzy@gmail.com>
2023-09-15 13:33:25 -0700

    python more data structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,10 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
 
+
 # Example usage:
-my_dict = {'a': 1, 'b': 2}
-update_dictionary(my_dict, 'c', 3)  # Add a new key-value pair
-update_dictionary(my_dict, 'b', 4)  # Update an existing key's value
-print(my_dict)
+if __name__ == "__main__":
+    my_dict = {'a': 1, 'b': 2}
+    update_dictionary(my_dict, 'c', 3)  # Add a new key-value pair
+    update_dictionary(my_dict, 'b', 4)  # Update an existing key's value
+    print(my_dict)
